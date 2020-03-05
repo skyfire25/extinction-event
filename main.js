3c8a5935e4920e2f6d63a7a327c8528b3f3d7849
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-03-05 16:16:24 -0500

    finished the button

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ const olLiFunk = function (event){
 for( const li of olLiQuery){
     li.addEventListener('click', olLiFunk)
 }
+
 const ulLiQuery = document.querySelectorAll('ul li');
 
 const ulLiFunk = function (event){
     event.target.style.opacity= '0.0';
     event.target.style.transition= '2s';
 }
+
 for( const li of ulLiQuery) {
     li.addEventListener('click', ulLiFunk);
 }
-const imagesQuery = document.querySelectorAll('#row');
+
+const imagesQuery = document.querySelectorAll('#row img');
 
 const imagesFunk = function (event){
     event.target.style.width= '0px';
@@ -25,15 +28,28 @@ const imagesFunk = function (event){
     //event.target.style.visibility= 'collapsed';
 
 }
+
 for( const row of imagesQuery) {
     row.addEventListener('click', imagesFunk);
 }
+
 const bottomMeteorMeQuery = document.querySelectorAll('#destroy-all')
 
-const bottomMeteorMe = function (event) {
-    olLiFunk(event);
-    ulLiFunk(event);
-    imagesFunk(event);
+const bottomMeteorMe = function () {
+    
+    for( const li of olLiQuery){
+        li.style.textDecoration= 'line-through'
+    }
+
+    for( const li of ulLiQuery) {
+        li.style.opacity= '0.0';
+        li.style.transition= '2s';
+    }
+
+    for( const row of imagesQuery) {
+        row.style.width= '0px';
+        row.style.transition= '2s';
+    }
 }
 
 for(const all of bottomMeteorMeQuery){
